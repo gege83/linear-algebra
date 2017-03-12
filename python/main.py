954be459102d6a27f810cc694aeae8362b9e59e4
Takács Gergő <gergo83@gmail.com>
2017-03-12 22:37:26 +0000

    implement plus, minus, normalization, dot product and angel check

--- python/main.py
+++ python/main.py
@@ -1,4 +1,43 @@
 from vector import *
 
-myVector = Vector([2,2,2])
-print myVector
+myVector1 = Vector([2,2,2])
+print myVector1
+
+myVector2 = Vector([2,2,3]);
+
+print myVector1+myVector2;
+print myVector1-myVector2;
+print 12*myVector1;
+print myVector1*12;
+print myVector1*myVector2;
+print myVector1.length();
+print myVector1.angle(myVector2);
+print myVector1.angleInDeg(myVector2);
+print myVector1.normalize()
+
+v1 = Vector([-2.328, -7.284, -1.214])
+w1 = Vector([-1.821, 1.072, -2.94])
+print v1.isOrthogonal(w1)
+print v1*w1
+print v1.isParalel(w1)
+print v1.angle(w1)
+
+
+v1 = Vector([3.039, 1.879])
+w1 = Vector([0.825, 2.036])
+
+v2 = Vector([-9.88, -3.264, -8.159])
+w2 = Vector([-2.155, -9.353, -9.473])
+
+v3 = Vector([3.009, -6.172, 3.692, -2.51])
+w3 = Vector([6.404, -9.144, 2.759, 8.718])
+
+print v1.projectTo(w1)
+print v1.projectToOrth(w1)
+
+print v2.projectTo(w2)
+print v2.projectToOrth(w2)
+print v2.projectTo(w2).isOrthogonal(v2.projectToOrth(w2))
+
+print v3.projectTo(w3)
+print v3.projectToOrth(w3)
--- python/vector.py
+++ python/vector.py
@@ -1,3 +1,6 @@
+from numbers import Number
+from math import *
+
 class Vector(object):
     def __init__(self, coordinates):
         try:
@@ -19,4 +22,67 @@ class Vector(object):
 
     def __eq__(self, v):
         return self.coordinates == v.coordinates
+
+    def __add__(self, other):
+        newCoords = [x-y for x,y in zip(self.coordinates, other.coordinates)]
+        return Vector(newCoords)
+
+    def __sub__(self, other):
+        newCoords = [x-y for x,y in zip(self.coordinates, other.coordinates)]
+        return Vector(newCoords)
+
+    def scalar(self, scalar):
+        newCoords = [x*scalar for x in self.coordinates]
+	return Vector(newCoords)
+
+    def innerProduct(self, other):
+        coordinateProducts = [x*y for x,y in zip(self.coordinates, other.coordinates)]
+        return sum(coordinateProducts)
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __mul__(self, other):
+        if isinstance(other, Number):
+            return self.scalar(other)
+        elif isinstance(other, Vector):
+            return self.innerProduct(other)
+
+    def length(self):
+        coordinateSquares = [x**2 for x in self.coordinates]
+        return sqrt(sum(coordinateSquares))
+
+    def normalize(self):
+	try:
+            length = self.length()
+	    return 1./length*self
+        except ZeroDivisionError:
+            raise Exception('cannot normalize the zero vector')
+
+    def angle(self, other):
+        prod = self.innerProduct(other)
+        result = prod / (self.length() * other.length())
+	return acos(result)
+
+    def angleInDeg(self, other):
+        return degrees(self.angle(other))
+
+    def isOrthogonal(self, other, tolerance=1e-10):
+        return abs(self*other)<tolerance
+
+    def isParalel(self, other):
+        if self.isZero() or other.isZero() :
+            return True
+        angle = self.angle(other)
+        return angle == 0 or angle == pi 
+
+    def isZero(self, tolerance=1e-10):
+        return self.length()<tolerance
+
+    def projectTo(self, base):
+        unit = base.normalize()
+	return (self*unit)*unit
+
+    def projectToOrth(self, base):
+        return self-self.projectTo(base)
 
